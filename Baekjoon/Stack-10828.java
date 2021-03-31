b8e8059281b76d2f3628620a9ce8ca30039a3e07
DaaEun <didekdms123@gmail.com>
2021-03-31 15:27:33 +0900

    stack 알고리즘 공부

---
+++ Baekjoon/Stack-10828.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Stack<Integer> stack = new Stack<>();
+		
+		Scanner sc = new Scanner(System.in);
+		StringBuilder sb = new StringBuilder();
+		int n = sc.nextInt();
+		
+		for(int i = 0; i < n; i++) {
+			String order = sc.next();
+
+			switch (order) {
+			case "push":
+				stack.push(sc.nextInt());
+				break;
+				
+			case "pop":
+				if(stack.empty()) {
+					sb.append(-1).append('\n');
+					break;
+				}
+				else {
+					sb.append(stack.peek()).append('\n');
+					stack.pop();
+					break;
+				}
+
+			case "size":
+				sb.append(stack.size()).append('\n');
+				break;
+				
+			case "empty":
+				sb.append(stack.empty() ? 1 : 0).append('\n');
+				break;
+				
+			case "top":
+				if(stack.empty()) {
+					sb.append(-1).append('\n');
+					break;
+				}
+				else {
+					sb.append(stack.peek()).append('\n');
+					break;
+				}
+			} //switch-end
+		} //for-end
+		System.out.println(sb);
+		sc.close();
+	}
+}
